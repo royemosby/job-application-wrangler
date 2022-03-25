bdc58ed0d9e07dd5545bea5189c308e064b52565
Roy Mosby <ego.madking@gmail.com>
2022-03-25 11:25:23 -0400

    adds associatations and validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,32 @@
 class Contact < ApplicationRecord
+
+  has_many :jobs
+  belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :contact_type, presence: true
+  validates :contact_type, inclusion: {in: ["recruiter", "personal", "peer", "manager", "employee", "flatiron alumni", "other"]}
+  validates :url, url: {allow_blank: true}
+
+  validate :validate_optional_email,
+    :validate_optional_phone
+
+  def validate_optional_email
+    if email.present?
+      unless !!email.match(URI::MailTo::EMAIL_REGEXP)
+        errors.add :email, 'email must either be empty or in valid format'
+      end
+    end
+  end
+
+  def validate_optional_phone
+    if phone.present?
+      # this isn't following NANP but good enough
+      unless !!phone.match(/\A(\d{3}-|\(\d{3}\))\d{3}-\d{4}\z/)
+        errors.add :phone, 'phone must be empty or in the following formats: nnn-nnn-nnnn or (nnn)nnn-nnnn. Place international numbers in notes.'
+      end
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :contacts
+  has_many :jobs
+
+  validates :email, presence: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :gh_uid, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, format: {with: /\A([a-zA-Z]|\d|_|-)*\z/, message: "only letters, numbers, dash or underscores allowed in usernames"}
+
 end
