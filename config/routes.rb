781943e0ca976ea5467c10dfeb035e1ee0568518
Roy Mosby <ego.madking@gmail.com>
2022-03-30 12:39:53 -0400

    removes gh auth routes + cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
+
   resources :contacts
   resources :jobs
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  get "/login", to: "sessions#new", as: "login"
-  post "/login", to: "sessions#create", as: "username_pw_login"
-  get "/auth/github/callback" => "sessions#create"
-  post "/logout", to: "session#destroy"
-  
-  root "sessions#new"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
 end
