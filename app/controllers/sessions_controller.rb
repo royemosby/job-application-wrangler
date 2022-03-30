b6120facb1b39281bba13ad19644cb43feb5c7a7
Roy Mosby <ego.madking@gmail.com>
2022-03-30 12:38:18 -0400

    changes auth to jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,57 +1,21 @@
-require "securerandom"
-
 class SessionsController < ApplicationController
 
-  def new
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-      redirect_to "/jobs", notice: "You are already logged on, #{@user.username}."
-    else
-      @user = User.new
-    end
-  end
+  skip_before_action :authenticated?, only: %i[create]
 
   def create
     if params.has_key? :username
-      if @user = User.find_by(username: params[:username])
-        if @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect_to "/jobs", success: "You have logged on, #{@user.username}"
-        end
+      @user = User.find_by(username: params[:username])
+      if  @user && @user.authenticate(params[:password])
+          @token = encode_token({user_id: @user.id})
+          render json: {user: @user, jwt: @token}
       else
-        redirect_to "/sessions#new", notice: "username not found and/or password was incorrect"
+        render json: {message: "Invalid username or password"}, status: :unauthorized
       end
-    # else
-      # @user = User.find_or_create_by(gh_uid: auth['uid']) do |u|
-      #   u.username = auth['info']['nickname']
-      #     u.email = auth['info']['email']
-      #     u.password = SecureRandom.base64(12)
-      # end
-      # session[:user_id] = @user.id
-      # redirect_to "/jobs", success: "You have been logged on, #{@user.username}."
     end
   end
 
+  #TODO: determine logout mechanism
   def destroy
-    if session[:user_id]
-      session.delete :user_id
-      redirect_to "/login", notice: "You have successfully logged out"
-    end
   end
 
-  private
-
-  def auth
-    request.env['omniauth.auth']
-  end
-
-  def current_user
-    @user = User.find_by(id: session[user_id])
-  end
-
-  def logged_on
-    unless current_user
-      redirect_to "/"
-    end
-  end
 end
