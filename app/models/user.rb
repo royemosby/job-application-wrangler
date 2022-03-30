8dd3a1e1f466ec6b82831fa812423af9dc64111a
Roy Mosby <ego.madking@gmail.com>
2022-03-30 12:39:18 -0400

    removes gh_uid validation for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
-  validates :gh_uid, uniqueness: true
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, format: {with: /\A([a-zA-Z]|\d|_|-)*\z/, message: "only letters, numbers, dash or underscores allowed in usernames"}
